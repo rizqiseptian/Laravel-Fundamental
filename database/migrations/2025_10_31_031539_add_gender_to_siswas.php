bfa787fc40b6eafa69a5e767dd24aeda46060c51
rizqiseptian <rizqiseptianramadan@gmail.com>
2025-10-31 10:40:25 +0700

    migration rename column

--- database/migrations/2025_10_31_031539_add_gender_to_siswas.php
+++ database/migrations/2025_10_31_031539_add_gender_to_siswas.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('siswas', function (Blueprint $table) {
-            //
+            $table->string('gender');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('siswas', function (Blueprint $table) {
-            //
+            $table->string('gender');
         });
     }
 };
